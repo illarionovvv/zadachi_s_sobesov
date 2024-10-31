b277bdea445fae2d55c706f92e0e16d79e9aafff
cekac <illarionovvv@gmail.com>
2024-11-01 02:36:36 +0300

    palindrome

---
+++ zadachi/palindrome.php
@@ -0,0 +1,32 @@
+<?php
+// полиндром
+
+
+echo isPalindrome('abbbba'). "\n";
+echo isPalindrome('aa'). "\n";
+echo isPalindrome('a'). "\n";
+echo isPalindrome('aaa'). "\n";
+echo isPalindrome('bbaab'). "\n"; // false
+
+
+function isPalindrome(string $string): int
+{
+$left = 0;
+$right = strlen($string)-1;
+
+$result = true;
+
+for ($i=0; $i < round( strlen($string)); $i++) {
+if ($left >= $right) {
+return 1;
+}
+
+if ($string[$left] != $string[$right]) {
+return 0;
+}
+$left++;
+$right--;
+}
+
+return (int)$result;
+}

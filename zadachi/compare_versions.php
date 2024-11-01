6545acd8de096299e25acb51683d0a68ab15cd16
v.illarionov <213ill@mail.ru>
2024-11-01 03:15:24 +0300

    compare versions lint

--- zadachi/compare_versions.php
+++ zadachi/compare_versions.php
@@ -8,28 +8,28 @@ print(version_cmp("1.01", "1.1")) . "\n"; // -> 0
 print(version_cmp("1.1.0", "1.1")) . "\n"; // -> 0
 function version_cmp(string $str1, string $str2)
 {
-$explodedString1 = explode('.', $str1);
-$explodedString2 = explode('.', $str2);
+    $explodedString1 = explode('.', $str1);
+    $explodedString2 = explode('.', $str2);
 
-$count2 = count($explodedString2);
-$count1 = count($explodedString1);
+    $count2 = count($explodedString2);
+    $count1 = count($explodedString1);
 
 
-$countMax = $count2 > $count1 ? $count2 : $count1;
+    $countMax = $count2 > $count1 ? $count2 : $count1;
 
-for ($j = 0; $j < $countMax; $j++) {
-$tmp1 = (isset($explodedString1[$j])) ? (int)$explodedString1[$j] : 0;
-$tmp2 = (isset($explodedString2[$j])) ? (int)$explodedString2[$j] : 0;
+    for ($j = 0; $j < $countMax; $j++) {
+        $tmp1 = (isset($explodedString1[$j])) ? (int)$explodedString1[$j] : 0;
+        $tmp2 = (isset($explodedString2[$j])) ? (int)$explodedString2[$j] : 0;
 
-if ($tmp1 >  $tmp2) {
-return 1;
-}
+        if ($tmp1 > $tmp2) {
+            return 1;
+        }
 
-if ($tmp1 < $tmp2) {
-return -1;
-}
-}
+        if ($tmp1 < $tmp2) {
+            return -1;
+        }
+    }
 
-return 0;
+    return 0;
 
 }
